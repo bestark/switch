19deff2e08b0f93d347d717d0cbd5a4a062201f3
Brian Stark <brian.joe.stark@gmail.com>
2020-02-26 20:34:04 +0100

    make Switch a functional component

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Board from "./Board";
 
-export default class Switch extends React.Component {
-    render() {
-        return (
-            <div className="game-board">
-                <Board/>
-            </div>
-        );
-    }
-}
+export default () => (
+    <div className="game">
+        <div className="game-board">
+            <Board />
+        </div>
+        <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+        </div>
+    </div>
+)
