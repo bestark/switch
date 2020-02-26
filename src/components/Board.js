8371cf60a73053d0cbc44402d210ece595cf9cd2
Brian Stark <brian.joe.stark@gmail.com>
2020-02-26 21:12:57 +0100

    add function to determine if nearby fields can be set

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,6 +10,26 @@ export default class Board extends React.Component {
         }
 
         this.handleClick = this.handleClick.bind(this);
+        this.canBeSet = this.canBeSet.bind(this);
+    }
+
+    canBeSet(fieldNumber, pos) {
+        if (pos === 'top' || pos === 'bottom') {
+            if (fieldNumber > 4 && fieldNumber < 20) {
+                return fieldNumber;
+            }
+        }
+
+        if (pos === 'right') {
+            if (fieldNumber !== 4 && fieldNumber !== 9 && fieldNumber !== 14 && fieldNumber !== 19 && fieldNumber !== 24) {
+                return fieldNumber;
+            }
+        }
+        if (pos === 'left') {
+            if (fieldNumber !== 0 && fieldNumber !== 5 && fieldNumber !== 10 && fieldNumber !== 15 && fieldNumber !== 20) {
+                return fieldNumber;
+            }
+        }
     }
 
     handleClick(e) {
@@ -19,30 +39,11 @@ export default class Board extends React.Component {
         // get All squares
         const squares = this.state.board.slice();
 
-        // set square in field
-        if(!!squares[elIndex]) {
-            //get nearby field
-            //no top
-            if(elIndex < 5) {
-                console.log('has no top');
-            }
+        const topEl = this.canBeSet(elIndex - 5, 'top');
+        const bottomEl = this.canBeSet(elIndex + 5, 'bottom');
+        const leftEl = this.canBeSet(elIndex - 1, 'left');
+        const rightEl = this.canBeSet(elIndex + 1, 'right');
 
-            // no right
-            if(elIndex === 4 || elIndex === 9 || elIndex === 14 || elIndex === 19 || elIndex === 24) {
-                console.log('has no right');
-            }
-
-            // no bottom
-            if(elIndex > 19) {
-                console.log('has no bottom');
-            }
-
-            // no left
-            if(elIndex === 0 || elIndex === 5 || elIndex === 10 || elIndex === 15 || elIndex === 20) {
-                console.log('has no left');
-            }
-
-        }
         squares[elIndex] = 'X';
         this.setState(() => ({
             board: squares
@@ -54,10 +55,11 @@ export default class Board extends React.Component {
         let row = [];
         let index = 0;
 
-        for(let i = 0; i< size; i++) {
+        for (let i = 0; i < size; i++) {
             let column = [];
             for (let j = 0; j < size; j++) {
-                column.push( <Square key={index} index={index} value={this.state.board[index]} onClick={this.handleClick}/>);
+                column.push(<Square key={index} index={index} value={this.state.board[index]}
+                                    onClick={this.handleClick}/>);
                 index++;
             }
             row.push(<div key={i} className={"board__row"}>{column}</div>)
