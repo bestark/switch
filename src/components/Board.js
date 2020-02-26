e3aee12d936febad2f0312c4689f78c70d6b1c41
Brian Stark <brian.joe.stark@gmail.com>
2020-02-26 20:28:15 +0100

    extend Square to render properly

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,14 @@ export default class Board extends React.Component {
 
     }
 
+    handleClick(e) {
+        const elIndex = e.target.getAttribute('data-index');
+        const squares = this.state.board.slice();
+        squares[elIndex] = 'X';
+        this.setState(() => ({
+            board: squares
+        }));
+    }
 
     createBoard(size = 5) {
         let board = [];
@@ -16,7 +24,7 @@ export default class Board extends React.Component {
         for(let i = 0; i< size; i++) {
             let column = [];
             for (let j = 0; j < size; j++) {
-                column.push( <Square key={index} />);
+                column.push( <Square key={index} index={index} value={this.state.board[index]} onClick={this.handleClick}/>);
                 index++;
             }
             row.push(<div key={i} className={"board__row"}>{column}</div>)
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick(e) {
+        this.props.onClick(e);
+    }
 
     render() {
         return (
-            <button className="square" >
+            <button className="square" data-index={this.props.index} onClick={this.handleClick}>
                 {this.props.value}
             </button>
         )
