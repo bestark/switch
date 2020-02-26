1b56af095f38afd90bd2e02e94d3aad156961ab7
Brian Stark <brian.joe.stark@gmail.com>
2020-02-26 20:27:08 +0100

    add createBoard method

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,35 +4,27 @@ import Square from "./Square";
 export default class Board extends React.Component {
     constructor(props) {
         super(props)
+
     }
 
-    renderSquare(i) {
-        return <Square value={i}/>
-    }
 
     createBoard(size = 5) {
         let board = [];
         let row = [];
-
-        // create columns
-
-
-        // create rows
         let index = 0;
+
         for(let i = 0; i< size; i++) {
             let column = [];
             for (let j = 0; j < size; j++) {
-                column.push(<Square key={index} value={index}/>)
+                column.push( <Square key={index} />);
                 index++;
             }
             row.push(<div key={i} className={"board__row"}>{column}</div>)
         }
 
-
         board.push(row);
+
         return board;
-
-
     }
 
     render() {
